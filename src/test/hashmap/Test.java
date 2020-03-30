7153469ac19f6498f79555b3d95c1730f30a7988
1064064 <boyangs@student.unimelb.edu.au>
2020-03-30 15:27:10 +0800

    initial_commit

---
+++ src/test/hashmap/Test.java
@@ -0,0 +1,11 @@
+package test.hashmap;
+
+import java.util.HashMap;
+
+public class Test {
+    public static void main(String[] args) {
+        HashMap<String,Integer> hashMap = new HashMap<>();
+        hashMap.put("a",1);
+        hashMap.put("b",2);
+    }
+}
